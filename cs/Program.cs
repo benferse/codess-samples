10e35c2a61d3aa9be981325ecd66ce7bb8430f8b
Ben <benferse@users.noreply.github.com>
2017-03-26 17:55:02 -0400

    Added boilerplate dotnet core sample project

---
+++ cs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Microsoft.Garage.Codess.Samples
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DelayedWriteLine("(should play the game of life here)");
+            DelayedWriteLine("But sadly we don't");
+        }
+
+        private static void DelayedWriteLine(string text)
+        {
+            try
+            {
+                Console.Clear();
+            }
+            catch (IOException)
+            {
+                // This should only really happen if the output is being redirected,
+                // and if that's true then we assume this is safe to ignore
+            }
+
+            Console.WriteLine(text);
+            Thread.Sleep(2000);
+        }
+    }
+}
